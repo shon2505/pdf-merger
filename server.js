1d7e0e164e0f7822d2c3f9b264098041d920a831
shon2505 <126013343+shon2505@users.noreply.github.com>
2024-02-10 14:40:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
 app.post('/merge', upload.array('pdfs', 2), async (req, res, next) =>{
     console.log(req.files)
    await mergePdfs(path.join(__dirname, req.files[0].path),path.join(__dirname, req.files[1].path))
-    res.redirect("http://localhost:3000/static/merged.pdf")
+    res.redirect("https://pdf-merger-8z9t.onrender.com/static/merged.pdf")
     // req.files is array of `photos` files
     // req.body will contain the text fields, if there were any
   })
